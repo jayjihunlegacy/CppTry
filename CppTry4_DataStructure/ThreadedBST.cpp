dfd617e90c09dd44ce7bd57e567cc2d34305b7c0
Jihun-Desktop <t080205@gmail.com>
2015-12-27 18:10:39 +0900

    xcv

---
+++ CppTry4_DataStructure/ThreadedBST.cpp
@@ -0,0 +1,9 @@
+#include "ThreadedBST.h"
+
+Node_T::Node_T(int val)
+{
+	val = value;
+	leftischild = rightischild = false;
+	left = right = NULL;
+}
+
---
+++ CppTry4_DataStructure/ThreadedBST.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdio.h>
+class Node_T
+{
+public:
+	Node_T(int);
+	
+	int value;
+	Node_T *left, *right;
+	bool leftischild, rightischild;
+};
+
+class BST_T
+{
+
+};
+
+namespace ThreadedBST
+{
+	Node_T * 
+}
